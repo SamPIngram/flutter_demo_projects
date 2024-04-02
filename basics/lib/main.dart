176a36156fe3ed65c12a1ec14b11d3be712fbca1
Sam Ingram <31179948+SamPIngram@users.noreply.github.com>
2024-04-02 21:26:04 +0100

    Section 2

---
+++ basics/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import "package:basics/gradient_container.dart";
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          const Color.fromARGB(255, 26, 2, 80),
+          const Color.fromARGB(255, 45, 7, 98),
+        ),
+      ),
+    ),
+  );
+}
